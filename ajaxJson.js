86af3211007da4d1495cfb2e619b8707d0e620c0
github4maninder <maninder.email@gmail.com>
2021-11-19 21:26:03 -0500

    add dynamic pageCounter

--- ajaxJson.js
+++ ajaxJson.js
@@ -1,17 +1,25 @@
+let pageCounter = 1;
+const dataContainer = document.getElementById('info')
 const btn = document.getElementById("btn");
 
 btn.addEventListener('click', function() {
   const ourRequest= new XMLHttpRequest();
-  ourRequest.open('GET','./data.json');
+  ourRequest.open('GET','./data'+ pageCounter +'.json');
   ourRequest.onload = function () {
-  const ourData =JSON.parse(ourRequest.responseText);
-    
+    const ourData =JSON.parse(ourRequest.responseText);
+    renderHTML(ourData);
   };
   ourRequest.send();
+  pageCounter++;
 });
 
-function renderHTML(){
-  
+function renderHTML(data){
+  let htmlString = "";
+
+  for( let i = 0; i < data.length; i++){
+    htmlString += "<p>" + data[i].name + "is a " + data[i].species+".";
+  }
+  info.insertAdjacentHTML("beforeend", htmlString);
 }
 
 
